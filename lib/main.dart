05a2d8dff5786368f84343c31ad236c9c96dfcd4
biryang <56025411+biryang@users.noreply.github.com>
2021-04-06 23:36:54 +0900

    2048 merge 로직 하드코딩

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,130 @@ class _MyHomePageState extends State<MyHomePage> {
   List<List<TileData>> grid = List.generate(
       4, (x) => List.generate(4, (y) => TileData(x: x, y: y, value: 0)));
 
+  List<List<int>> gridInt = List.generate(4, (x) => List.generate(4, (y) => 2));
+
+  void addTile() {
+    for (int x = 0; x < 4; x++) {
+      for (int y = 0; y < 4; y++) {
+        if (gridInt[x][y] == 0) {
+          gridInt[x][y] = 2;
+          print(gridInt);
+          return;
+        }
+      }
+    }
+  }
+
+  void mergeUp() {
+    print('be');
+    print(gridInt);
+    for (int y = 0; y < 3; y++) {
+      for (int x = 0; x < 4; x++) {
+        if (gridInt[y][x] == gridInt[y + 1][x]) {
+          gridInt[y][x] = gridInt[y][x] + gridInt[y + 1][x];
+          gridInt[y + 1][x] = 0;
+        }
+      }
+    }
+    for (int n = 0; n < 4; n++) {
+      for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 4; x++) {
+          if (gridInt[y][x] == 0) {
+            gridInt[y][x] = gridInt[y + 1][x];
+            gridInt[y + 1][x] = 0;
+          }
+        }
+      }
+    }
+    print('af');
+    gridInt.forEach((element) {
+      print(element);
+    });
+  }
+
+  void mergeDown() {
+    print('be');
+    print(gridInt);
+    for (int y = 3; y > 0; y--) {
+      for (int x = 0; x < 4; x++) {
+        if (gridInt[y][x] == gridInt[y - 1][x]) {
+          gridInt[y][x] = gridInt[y][x] + gridInt[y - 1][x];
+          gridInt[y - 1][x] = 0;
+        }
+      }
+    }
+    for (int n = 0; n < 4; n++) {
+      for (int y = 3; y > 0; y--) {
+        for (int x = 0; x < 4; x++) {
+          if (gridInt[y][x] == 0) {
+            gridInt[y][x] = gridInt[y - 1][x];
+            gridInt[y - 1][x] = 0;
+          }
+        }
+      }
+    }
+    print('af');
+    gridInt.forEach((element) {
+      print(element);
+    });
+  }
+
+  void mergeLeft() {
+    print('be');
+    print(gridInt);
+    for (int y = 0; y < 3; y++) {
+      for (int x = 0; x < 4; x++) {
+        if (gridInt[x][y] == gridInt[x][y + 1]) {
+          gridInt[x][y] = gridInt[x][y] + gridInt[x][y + 1];
+          gridInt[x][y + 1] = 0;
+        }
+      }
+    }
+    for (int n = 0; n < 4; n++) {
+      for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 4; x++) {
+          if (gridInt[x][y] == 0) {
+            gridInt[x][y] = gridInt[x][y + 1];
+            gridInt[x][y + 1] = 0;
+          }
+        }
+      }
+    }
+    print('af');
+    gridInt.forEach((element) {
+      print(element);
+    });
+  }
+
+  void mergeRight() {
+    print('be');
+    print(gridInt);
+    for (int y = 3; y > 0; y--) {
+      for (int x = 0; x < 4; x++) {
+        if (gridInt[x][y] == gridInt[x][y - 1]) {
+          gridInt[x][y] = gridInt[x][y] + gridInt[x][y - 1];
+          gridInt[x][y - 1] = 0;
+        }
+      }
+    }
+    for (int n = 0; n < 4; n++) {
+      for (int y = 3; y > 0; y--) {
+        for (int x = 0; x < 4; x++) {
+          if (gridInt[x][y] == 0) {
+            gridInt[x][y] = gridInt[x][y - 1];
+            gridInt[x][y - 1] = 0;
+          }
+        }
+      }
+    }
+    print('af');
+    gridInt.forEach((element) {
+      print(element);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('grid');
-    print(grid);
     final gridSize = MediaQuery.of(context).size.width - 16 * 2;
     final tileSize = (gridSize - 4.0 * 2) / 4;
     List<Widget> stackItems = [];
@@ -57,8 +177,28 @@ class _MyHomePageState extends State<MyHomePage> {
             borderRadius: BorderRadius.circular(8),
             color: Colors.brown[100],
           ),
-          child: Stack(
-            children: stackItems,
+          child: GestureDetector(
+            onVerticalDragEnd: (details) {
+              if (details.velocity.pixelsPerSecond.dy < -250) {
+                print('up');
+                mergeUp();
+              } else if (details.velocity.pixelsPerSecond.dy > 250) {
+                print('down');
+                mergeDown();
+              }
+            },
+            onHorizontalDragEnd: (details) {
+              if (details.velocity.pixelsPerSecond.dx < -1000) {
+                print('left');
+                mergeLeft();
+              } else if (details.velocity.pixelsPerSecond.dx > 1000) {
+                print('right');
+                mergeRight();
+              }
+            },
+            child: Stack(
+              children: stackItems,
+            ),
           ),
         ),
       ),
